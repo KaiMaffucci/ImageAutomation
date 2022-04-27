7535bf1fa9a4c9c2c60527d3b3b5475accd32898
KaiMaffucci <kai.maffucci@gmail.com>
2022-04-27 13:01:25 -0400

    removed legacy file

--- usb_wait.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# we could make a version which doesn't need this file
-# but it does make things slightly clearer, so I'l leave it for now
-
-read -p "Plug in 2nd USB stick and press enter..."
